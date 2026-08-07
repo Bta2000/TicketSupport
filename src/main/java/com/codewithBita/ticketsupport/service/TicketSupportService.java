f7a37593c7653092d3edecb8243649b35e7fc035
bita <user23.tfa@gamil.com>
2026-08-07 10:45:25 +0330

    feat: add ticket repository and service layer & implement necessary method in service class

---
+++ src/main/java/com/codewithBita/ticketsupport/service/TicketSupportService.java
@@ -0,0 +1,52 @@
+package com.codewithBita.ticketsupport.service;
+
+import com.codewithBita.ticketsupport.entity.TicketSupportEntity;
+import com.codewithBita.ticketsupport.enums.TicketStatus;
+import com.codewithBita.ticketsupport.mapper.TicketSupportMapper;
+import com.codewithBita.ticketsupport.model.TicketSupportModel;
+import com.codewithBita.ticketsupport.repository.TicketSupportRepository;
+//import com.codewithBita.ticketsupport.validator.TicketSupportValidator;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class TicketSupportService {
+
+
+    private final TicketSupportRepository ticketSupportRepository;
+    private final TicketSupportMapper ticketSupportMapper;
+//    private final TicketSupportValidator ticketSupportValidator;
+
+
+    @Transactional(rollbackFor = Exception.class)
+    public TicketSupportModel create(TicketSupportModel model) {
+
+//        ticketSupportValidator.validate(model);
+        TicketSupportEntity entity = ticketSupportMapper.modelToEntity(model);
+        ticketSupportRepository.save(entity);
+        return ticketSupportMapper.entityToModel(entity);
+
+    }
+
+    @Transactional(readOnly = true)
+    public TicketSupportModel getById(Long id) {
+        return ticketSupportMapper.entityToModel(ticketSupportRepository.getById(id));
+    }
+
+    @Transactional(readOnly = true)
+    public Page<TicketSupportModel> getListOfTickets(Pageable pageable) {
+        Page<TicketSupportEntity> entities = ticketSupportRepository.findAll(pageable);
+        return entities.map(ticketSupportMapper::entityToModel);
+    }
+
+    @Transactional(rollbackFor = Exception.class)
+    public void updateTicketStatus(Long id , TicketStatus status) {
+        TicketSupportEntity entity = ticketSupportRepository.findById(id).orElseThrow();
+        if (status != null)
+            entity.setStatus(status);
+    }
+}
